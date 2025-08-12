11c99b3ff6b23b3a8251ebe31da29d4baa271db6
XIO <100460377+TazkieCT@users.noreply.github.com>
2025-08-12 19:11:13 +0700

    feat: add social media

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { HyperText } from "../components/magicui/hyper-text";
 import Navbar from "../components/navbar";
+import SocialMedia from "../components/social-media";
 
 export default function Home() {
   return (
@@ -7,9 +8,10 @@ export default function Home() {
       <div className="absolute top-0 left-0 w-full">
         <Navbar />
       </div>
+      <SocialMedia />
       <main className="max-w-4xl mx-auto px-4 py-12 min-h-screen flex flex-col items-center justify-center">
-        <HyperText  className="text-4xl font-bold text-center text-gray-900">Hello 👋, I'm Tazkie</HyperText>
-        <p className="text-lg text-gray-600 text-center mt-4">On my inner journey to grow as a mobile & web developer, one project at a time</p>
+        <HyperText  className="text-4xl font-bold text-center text-gray-900">Hello 👋 I'm Tazkie</HyperText>
+        <p className="text-lg text-gray-600 text-center mt-4">On my journey to grow as a mobile & web developer.</p>
         {/* <p className="text-lg text-gray-600 text-center mt-4">
           A curious coder turning <span className="text-emerald-600">tea 🍵</span> into <span className="text-indigo-600">code 💻</span> (and learning along the way)
         </p> */}
